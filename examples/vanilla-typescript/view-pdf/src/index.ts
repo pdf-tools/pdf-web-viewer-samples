ac0ec241868d10ee0a6e15345fd0337a94188451
ekansh-anand-pdftools <ekansh.anand@pdftools.com>
2025-08-25 11:28:44 +0200

    Update index.ts
    Updated viewer initialization

--- examples/vanilla-typescript/view-pdf/src/index.ts
+++ examples/vanilla-typescript/view-pdf/src/index.ts
@@ -1,7 +1,7 @@
 import { PdfToolsViewer } from '@pdftools/pdf-web-viewer';
 
 async function init() {
-  const pdfWebViewer = await PdfToolsViewer.init();
+  const pdfWebViewer = await PdfToolsViewer.initialize();
   const container = document.getElementById('viewer-container');
   container.append(pdfWebViewer);
 }
